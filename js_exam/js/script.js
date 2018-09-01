5d9eb5b108e7457c686a2ebda3e6c1a48caa6ebc
Tasha <tasha.ipatova@gmail.com>
2018-09-01 14:17:28 +0300

    general updates

--- js_exam/js/script.js
+++ js_exam/js/script.js
@@ -17,13 +17,13 @@ $(document).ready(function () {
 
 
     /*====================== ПОДКЛЮЧЕНИЕ SLICK SLIDER =====================*/
-    /*$('.single-item').slick();*/
-    $('.container').slick({
+    $('.slick_team').slick();
+    /*$('.container').slick({
 
 
 
-        /*prevArrow:'<img class="prev" src="img/back_arrow.png">',
-        nextArrow:'<img class="next" src="img/forward_arrow.png">',*/
+        /!*prevArrow:'<img class="prev" src="img/back_arrow.png">',
+        nextArrow:'<img class="next" src="img/forward_arrow.png">',*!/
 
         centerMode: true,
         centerPadding: '60px',
@@ -49,25 +49,64 @@ $(document).ready(function () {
             }
         ]
 
-    });
+    });*/
     /*====================== END OF SLICK SLIDER =====================*/
 
 
     /*====================== ISOTOPE =====================*/
-    var $isotopeGrid = $('.portfolio-images');
-
-    $isotopeGrid.isotope({
-        itemSelector: '.img-wrapper',
-        percentPosition: true
+    // init Isotope
+    var $grid = $('.grid').isotope({
+        itemSelector: '.element-item',
+        layoutMode: 'fitRows',
+        getSortData: {
+            name: '.name',
+            symbol: '.symbol',
+            number: '.number parseInt',
+            category: '[data-category]',
+            weight: function( itemElem ) {
+                var weight = $( itemElem ).find('.weight').text();
+                return parseFloat( weight.replace( /[\(\)]/g, '') );
+            }
+        }
     });
 
-    $('.portfolio .filters button').click(function () {
-        var filterValue = $(this).data('filter');
+// filter functions
+    var filterFns = {
+        // show if number is greater than 50
+        numberGreaterThan50: function() {
+            var number = $(this).find('.number').text();
+            return parseInt( number, 10 ) > 50;
+        },
+        // show if name ends with -ium
+        ium: function() {
+            var name = $(this).find('.name').text();
+            return name.match( /ium$/ );
+        }
+    };
 
-        $isotopeGrid.isotope({
-            filter: filterValue
+// bind filter button click
+    $('#filters').on( 'click', 'button', function() {
+        var filterValue = $( this ).attr('data-filter');
+        // use filterFn if matches value
+        filterValue = filterFns[ filterValue ] || filterValue;
+        $grid.isotope({ filter: filterValue });
+    });
+
+// bind sort button click
+    $('#sorts').on( 'click', 'button', function() {
+        var sortByValue = $(this).attr('data-sort-by');
+        $grid.isotope({ sortBy: sortByValue });
+    });
+
+// change is-checked class on buttons
+    $('.button-group').each( function( i, buttonGroup ) {
+        var $buttonGroup = $( buttonGroup );
+        $buttonGroup.on( 'click', 'button', function() {
+            $buttonGroup.find('.is-checked').removeClass('is-checked');
+            $( this ).addClass('is-checked');
         });
     });
+
     /*====================== END OF ISOTOPE =====================*/
 
 
@@ -75,8 +114,8 @@ $(document).ready(function () {
 
 
 /* =========================================== GOOGLE MAPS =========================================== */
-    var coordLat = 51.4868729,
-        coordLng = -0.0859594;
+    var coordLat = 49.568583,
+        coordLng = 34.585416;
 // Initialize and add the map
 function initMap() {
     // The location of requiredPlacement
